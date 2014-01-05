c63fc99710f4600dc35e05ff4b5dd7bac35a8a52
Armterla <70601797@qq.com>
2014-01-06 01:47:43 +0800

    designment of hd and id.

--- GAIA/gaia_hd.h
+++ GAIA/gaia_hd.h
@@ -5,9 +5,38 @@ namespace GAIA
 {
 	namespace FRAMEWORK
 	{
-		class HD
+		class Instance;
+		class HD // Handle class.
 		{
 		public:
+			ENUM_BEGIN(RIGHT)
+				RIGHT_READ			= 0x00000001,
+				RIGHT_WRITE			= 0x00000002,
+			ENUM_END(RIGHT)
+		public:
+			GINL HD();
+			GINL HD(const HD& src); // Auto reference count increase.
+			GINL ~HD(); // Auto reference count decrease.
+			GINL GAIA::GVOID Invalid();
+			GINL GAIA::BL IsInvalid() const;
+			GINL GAIA::BL SetRight(UM uRight) const;
+			GINL GAIA::UM GetRight() const;
+			GINL GAIA::BL Close() const;
+			GINL HD MakeShadow() const;
+			GINL GAIA::BL IsShadow() const;
+			GINL GAIA::FRAMEWORK::Instance* GetInstance() const;
+
+			GINL const HD& operator = (const HD& src); // Auto reference count increase.
+			GINL const BL operator == (const HD& src) const;
+			GINL const BL operator != (const HD& src) const;
+			GINL const BL operator >= (const HD& src) const;
+			GINL const BL operator <= (const HD& src) const;
+			GINL const BL operator > (const HD& src) const;
+			GINL const BL operator < (const HD& src) const;
+		private:
+			HD* m_pPrevHD;
+			HD* m_pNextHD;
+			GAIA::FRAMEWORK::Instance* m_pInstance;
 		};
 	};
 };
--- GAIA/gaia_id.h
+++ GAIA/gaia_id.h
@@ -8,6 +8,24 @@ namespace GAIA
 		class ID
 		{
 		public:
+			GINL ID();
+			GINL ~ID();
+			GINL GAIA::GVOID Invalid();
+			GINL GAIA::BL IsInvalid() const;
+			GINL GAIA::BL IsStatic() const;
+			GINL  const ID& operator = (const ID& src);
+			GINL  const GAIA::BL operator == (const ID& src) const;
+			GINL  const GAIA::BL operator != (const ID& src) const;
+			GINL  const GAIA::BL operator >= (const ID& src) const;
+			GINL  const GAIA::BL operator <= (const ID& src) const;
+			GINL  const GAIA::BL operator > (const ID& src) const;
+			GINL  const GAIA::BL operator < (const ID& src) const;
+		private:
+			GAIA::U32 m_uIndex0;
+			GAIA::U32 m_uIndex1;
+			GAIA::U32 m_uIndex2;
+			GAIA::U16 m_uGroup;
+			GAIA::U16 m_uStaticCheckField;
 		};
 	};
 };
