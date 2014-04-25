0f00dd25dfb667dcb8c3485cf581d9221859fb15
Armterla <70601797@qq.com>
2014-04-25 17:16:40 +0800

    iterator support ! operator.

--- GAIA/gaia_iterator.h
+++ GAIA/gaia_iterator.h
@@ -17,6 +17,7 @@ namespace GAIA
 			virtual Iterator<_DataType>& operator = (const Iterator<_DataType>& src) = 0;
 			virtual GAIA::BL operator == (const Iterator<_DataType>& src) const = 0;
 			virtual GAIA::BL operator != (const Iterator<_DataType>& src) const = 0;
+			virtual GAIA::BL operator ! (){return this->empty();}
 		private:
 			virtual Iterator<_DataType>& operator ++ (GAIA::N32) = 0;
 			virtual Iterator<_DataType>& operator -- (GAIA::N32) = 0;
@@ -33,6 +34,7 @@ namespace GAIA
 			virtual ConstIterator<_DataType>& operator = (const ConstIterator<_DataType>& src) = 0;
 			virtual GAIA::BL operator == (const ConstIterator<_DataType>& src) const = 0;
 			virtual GAIA::BL operator != (const ConstIterator<_DataType>& src) const = 0;
+			virtual GAIA::BL operator ! (){return this->empty();}
 		private:
 			virtual ConstIterator<_DataType>& operator ++ (GAIA::N32) = 0;
 			virtual ConstIterator<_DataType>& operator -- (GAIA::N32) = 0;
