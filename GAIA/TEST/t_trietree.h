822929f873337ba5ff8e2a2473b991b4fb8a8f6c
Armterla <70601797@qq.com>
2014-04-30 01:08:28 +0800

    write trie test case go a step further.

--- GAIA/TEST/t_trietree.h
+++ GAIA/TEST/t_trietree.h
@@ -6,6 +6,36 @@ namespace GAIATEST
 	GINL GAIA::N32 t_trietree(GAIA::FILESYSTEM::File& file, GAIA::PRINT::Print& prt)
 	{
 		GAIA::N32 nRet = 0;
+
+		typedef GAIA::CONTAINER::BasicTrieTree<GAIA::N32, GAIA::U32, GAIA::ALGORITHM::TwiceSizeIncreaser<GAIA::U32> > __TrieType;
+		__TrieType t;
+		if(!t.empty())
+		{
+			GTLINE2("New trie is not empty!");
+			++nRet;
+		}
+		if(t.size())
+		{
+			GTLINE2("New trie size is not zero!");
+			++nRet;
+		}
+		if(t.count(t.root()) != 0)
+		{
+			GTLINE2("Trie root's count not zero!");
+			++nRet;
+		}
+		if(t.catagory_count(t.root()) != 0)
+		{
+			GTLINE2("Trie root's catagory_count not zero!");
+			++nRet;
+		}
+		if(t.full_count(t.root()) != 0)
+		{
+			GTLINE2("Trie root's full_count not zero!");
+			++nRet;
+		}
+		t.clear();
+		t.destroy();
 		return nRet;
 	}
 };
