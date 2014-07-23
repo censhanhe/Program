e2f146e2c09e4a1b3718b08e7457e9db9cb94f92
Armterla <70601797@qq.com>
2014-07-24 01:06:26 +0800

    change multi bytes and wide chars convert function name

--- GAIA/gaia_locale.h
+++ GAIA/gaia_locale.h
@@ -7,8 +7,8 @@ namespace GAIA
 	{
 		GAIA_DEBUG_CODEPURE_FUNC const GAIA::GCH* strlocale(const GAIA::GCH* pszLocalName); // If pszLocalName == "", will set the default locale.
 		GAIA_DEBUG_CODEPURE_FUNC const GAIA::GCH* strlocale();
-		GAIA_DEBUG_CODEPURE_FUNC GAIA::SIZE str2wcs(const GAIA::GCH* pszSrc, GAIA::GWCH* pszDst, GAIA::SIZE dst_size_in_wchar);
-		GAIA_DEBUG_CODEPURE_FUNC GAIA::SIZE str2mbs(const GAIA::GWCH* pszSrc, GAIA::GCH* pszDst, GAIA::SIZE dst_size_in_bytes);
+		GAIA_DEBUG_CODEPURE_FUNC GAIA::SIZE m2w(const GAIA::GCH* pszSrc, GAIA::GWCH* pszDst, GAIA::SIZE dst_size_in_wchar);
+		GAIA_DEBUG_CODEPURE_FUNC GAIA::SIZE w2m(const GAIA::GWCH* pszSrc, GAIA::GCH* pszDst, GAIA::SIZE dst_size_in_bytes);
 	};
 };
 
--- GAIA/gaia_locale_indp.h
+++ GAIA/gaia_locale_indp.h
@@ -10,8 +10,8 @@ namespace GAIA
 	{
 		GAIA_DEBUG_CODEPURE_FUNC const GAIA::GCH* strlocale(const GAIA::GCH* pszLocalName){GAIA_AST(pszLocalName != GNULL); return setlocale(LC_ALL, pszLocalName);}
 		GAIA_DEBUG_CODEPURE_FUNC const GAIA::GCH* strlocale(){return setlocale(LC_ALL, NULL);}
-		GAIA_DEBUG_CODEPURE_FUNC GAIA::SIZE str2wcs(const GAIA::GCH* pszSrc, GAIA::GWCH* pszDst, GAIA::SIZE dst_size_in_wchar){return mbstowcs(pszDst, pszSrc, dst_size_in_wchar);}
-		GAIA_DEBUG_CODEPURE_FUNC GAIA::SIZE str2mbs(const GAIA::GWCH* pszSrc, GAIA::GCH* pszDst, GAIA::SIZE dst_size_in_bytes){return wcstombs(pszDst, pszSrc, dst_size_in_bytes);}
+		GAIA_DEBUG_CODEPURE_FUNC GAIA::SIZE m2w(const GAIA::GCH* pszSrc, GAIA::GWCH* pszDst, GAIA::SIZE dst_size_in_wchar){return mbstowcs(pszDst, pszSrc, dst_size_in_wchar);}
+		GAIA_DEBUG_CODEPURE_FUNC GAIA::SIZE w2m(const GAIA::GWCH* pszSrc, GAIA::GCH* pszDst, GAIA::SIZE dst_size_in_bytes){return wcstombs(pszDst, pszSrc, dst_size_in_bytes);}
 	};
 };
 
