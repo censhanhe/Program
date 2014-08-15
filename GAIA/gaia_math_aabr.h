ef078106bce2adfee8deb1f94f196d17ddb18766
Armterla <70601797@qq.com>
2014-08-15 16:12:38 +0800

    gaia math aabb and aabr implement go a step further

--- GAIA/gaia_math_aabr.h
+++ GAIA/gaia_math_aabr.h
@@ -16,20 +16,16 @@ namespace GAIA
 			GINL AABR(const __MyType& src){this->operator = (src);}
 			template<typename _ParamDataType> AABR(const _ParamDataType* p){this->operator = (p);}
 			template<typename _ParamDataType> AABR(const _ParamDataType& t){this->operator = (t);}
-
 			GINL GAIA::GVOID identity(){pmin = (_DataType)+1; pmax = (_DataType)-1;}
 			GINL GAIA::BL isidentity() const{return pmin.x > pmax.x || pmin.y > pmax.y;}
-
 			GINL GAIA::MATH::VEC2<_DataType> center() const{return (pmin + pmax) / (_DataType)2;}
 			GINL GAIA::MATH::VEC2<_DataType> size() const{return pmax - pmin;}
 			GINL _DataType long_radius() const{(pmax - pmin).length() / (_DataType)2;}
 			GINL _DataType short_radius() const{(pmax - pmin).minimize() / (_DataType)2;}
 			GINL _DataType width() const{return pmax.x - pmin.x;}
 			GINL _DataType height() const{return pmax.y - pmin.y;}
-
 			template<typename _ParamDataType> __MyType& expand(const GAIA::MATH::AABR<_ParamDataType>& t){pmin.minimize(t.pmin); pmax.maximize(t.pmax);}
 			template<typename _ParamDataType> __MyType& expand(const GAIA::MATH::VEC2<_ParamDataType>& t){pmin.minimize(t); pmax.maximize(t);}
-
 			template<typename _ParamDataType> __MyType operator + (const GAIA::MATH::VEC2<_ParamDataType>& v){__MyType ret; ret.pmin = pmin + v; ret.pmax = pmax + v; return ret;}
 			template<typename _ParamDataType> __MyType operator - (const GAIA::MATH::VEC2<_ParamDataType>& v){__MyType ret; ret.pmin = pmin - v; ret.pmax = pmax - v; return ret;}
 			template<typename _ParamDataType> __MyType operator * (const GAIA::MATH::VEC2<_ParamDataType>& v){__MyType ret; ret.pmin = pmin * v; ret.pmax = pmax * v; return ret;}
@@ -38,20 +34,43 @@ namespace GAIA
 			template<typename _ParamDataType> __MyType& operator -= (const GAIA::MATH::VEC2<_ParamDataType>& v){pmin -= v; pmax -= v; return *this;}
 			template<typename _ParamDataType> __MyType& operator *= (const GAIA::MATH::VEC2<_ParamDataType>& v){pmin *= v; pmax *= v; return *this;}
 			template<typename _ParamDataType> __MyType& operator /= (const GAIA::MATH::VEC2<_ParamDataType>& v){pmin /= v; pmax /= v; return *this;}
-
-			template<typename _ParamDataType> _DataType distance(const __MyType& src) const
+			template<typename _ParamDataType> GAIA::MATH::VEC2<_DataType> nearest_point(const GAIA::MATH::VEC2<_ParamDataType>& v) const
 			{
+				GAIA::MATH::VEC2<_DataType> ret;
+				if(v.x < pmin.x)
+					ret.x = pmin.x;
+				else if(v.x > pmax.x)
+					ret.x = pmax.x;
+				if(v.y < pmin.y)
+					ret.y = pmin.y;
+				else if(v.y > pmax.y)
+					ret.y = pmax.y;
+				return ret;
 			}
-			template<typename _ParamDataType> _DataType distancesq(const __MyType& src) const
+			template<typename _ParamDataType> _DataType distance(const GAIA::MATH::AABR<_ParamDataType>& src) const{return GAIA::MATH::xsqrt(this->distancesq(src));}
+			template<typename _ParamDataType> _DataType distancesq(const GAIA::MATH::AABR<_ParamDataType>& src) const
 			{
+				_DataType d[4];
+				d[0] = GAIA::MATH::xabs(pmin.x - src.pmin.x);
+				d[1] = GAIA::MATH::xabs(pmin.x - src.pmax.x);
+				d[3] = GAIA::MATH::xabs(pmax.x - src.pmin.x);
+				d[4] = GAIA::MATH::xabs(pmax.x - src.pmax.x);
+				_DataType nearest_x = GAIA::ALGORITHM::minimize(
+					GAIA::ALGORITHM::minimize(d[0], d[1]), GAIA::ALGORITHM::minimize(d[2], d[3]));
+				d[0] = GAIA::MATH::xabs(pmin.y - src.pmin.y);
+				d[1] = GAIA::MATH::xabs(pmin.y - src.pmax.y);
+				d[3] = GAIA::MATH::xabs(pmax.y - src.pmin.y);
+				d[4] = GAIA::MATH::xabs(pmax.y - src.pmax.y);
+				_DataType nearest_y = GAIA::ALGORITHM::minimize(
+					GAIA::ALGORITHM::minimize(d[0], d[1]), GAIA::ALGORITHM::minimize(d[2], d[3]));
+				return nearest_x * nearest_x + nearest_y * nearest_y;
 			}
-			template<typename _ParamDataType> _DataType distance(const GAIA::MATH::VEC2<_ParamDataType>& v) const
-			{
-			}
+			template<typename _ParamDataType> _DataType distance(const GAIA::MATH::VEC2<_ParamDataType>& v) const{return GAIA::MATH::xsqrt(this->distancesq(v));}
 			template<typename _ParamDataType> _DataType distancesq(const GAIA::MATH::VEC2<_ParamDataType>& v) const
 			{
+				GAIA::MATH::VEC2<_DataType> nearestpoint = this->nearest_point(v);
+				return (v - nearest_point).lengthsq();
 			}
-
 			template<typename _ParamDataType> GAIA::INTERSECT_TYPE intersect_point(const GAIA::MATH::VEC2<_ParamDataType>& v) const
 			{
 			}
@@ -62,11 +81,9 @@ namespace GAIA
 			{
 				GAIA_AST(pPlanes != GNULL);
 			}
-
 			template<typename _ParamDataType> __MyType& operator = (const GAIA::MATH::AABR<_ParamDataType>& src){pmin = src.pmin; pmax = src.pmax; return *this;}
 			template<typename _ParamDataType> __MyType& operator = (const _ParamDataType* p){pmin = p; pmax = p + 2; return *this;}
 			template<typename _ParamDataType> __MyType& operator = (const _ParamDataType& v){pmin = v; pmax = v; return *this;}
-
 			template<typename _ParamDataType> GAIA::BL operator == (const GAIA::MATH::AABR<_ParamDataType>& t) const{return pmin == t.pmin && pmax == t.pmax;}
 			template<typename _ParamDataType> GAIA::BL operator != (const GAIA::MATH::AABR<_ParamDataType>& t) const{return !(this->operator = (t));}
 			template<typename _ParamDataType> GAIA::BL operator >= (const GAIA::MATH::AABR<_ParamDataType>& t) const
@@ -89,7 +106,6 @@ namespace GAIA
 			template<typename _ParamDataType> GAIA::BL operator < (const GAIA::MATH::AABR<_ParamDataType>& t) const{return !(this->operator >= (t));}
 			template<typename _ParamDataType> const _DataType& operator [] (const _ParamDataType& index) const{return ((_DataType*)this)[index];}
 			template<typename _ParamDataType> _DataType& operator [] (const _ParamDataType& index){return ((_DataType*)this)[index];}
-
 		private:
 			GINL GAIA::GVOID selfupdate()
 			{
@@ -98,7 +114,6 @@ namespace GAIA
 				if(pmin.y > pmax.y)
 					GAIA::ALGORITHM::swap(pmin.y, pmax.y);
 			}
-
 		public:
 			GAIA::MATH::VEC2<_DataType> pmin;
 			GAIA::MATH::VEC2<_DataType> pmax;
