41afa7753f07767ff6488bf869a37869a7bedd19
Armterla <70601797@qq.com>
2014-09-28 16:24:04 +0800

    goon

--- GAIA/TEST/t_render_gdiplus.h
+++ GAIA/TEST/t_render_gdiplus.h
@@ -6,6 +6,31 @@ namespace GAIATEST
 	GINL GAIA::N32 t_render_gdiplus(GAIA::FSYS::File& file, GAIA::PRINT::PrintBase& prt)
 	{
 		GAIA::N32 nRet = 0;
+
+		/* Create factory. */
+		GAIA::FWORK::Factory fac;
+		typedef GAIA::RENDER::Render2DGDIPlus __RenderType;
+
+		/* Create convas. */
+		GAIA::UI::Canvas* pCanvas = dynamic_cast<GAIA::UI::Canvas*>(fac.CreateInstance(GAIA::FWORK::CLSID_CANVAS, GNIL));
+		GAIA_AST(pCanvas != GNIL);
+
+		/* Create render. */
+		__RenderType* p = dynamic_cast<__RenderType*>(fac.CreateInstance(GAIA::FWORK::CLSID_RENDER_2D_GDIPLUS, GNIL));
+		GAIA_AST(p != GNIL);
+
+		/* Initialize render. */
+
+		/* Release render. */
+
+		/* Delete render. */
+		p->Release();
+		p = GNIL;
+
+		/* Delete canvas. */
+		pCanvas->Release();
+		pCanvas = GNIL;
+
 		return nRet;
 	}
 };
