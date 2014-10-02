9363171341f82bd9272b28db2601538f96803b4a
Armterla <70601797@qq.com>
2014-10-02 16:00:21 +0800

    gaia factory impl

--- GAIA/gaia_factory.h
+++ GAIA/gaia_factory.h
@@ -16,11 +16,11 @@ namespace GAIA
 		private: // Helper type declaration here for internal use.
 			typedef GAIA::CTN::Vector<FactoryCreateCallBack*> LIST_CREATECALLBACK;
 		public:
-			GINL Factory(){}
+			GINL Factory(){this->init();}
 			GINL ~Factory(){}
 			GINL GAIA::BL RegistCreateCallBack(FactoryCreateCallBack* pCallBack)
 			{
-				if(m_listCreateCB.find(pCallBack, 0) != (LIST_CREATECALLBACK::_sizetype)-1)
+				if(m_listCreateCB.find(pCallBack, 0) != GINVALID)
 					return GAIA::False;
 				m_listCreateCB.push_back(pCallBack);
 				return GAIA::True;
@@ -31,7 +31,7 @@ namespace GAIA
 				if(pCallBack == GNIL)
 					return GAIA::False;
 				LIST_CREATECALLBACK::_sizetype t = m_listCreateCB.find(pCallBack, 0);
-				if(t == (LIST_CREATECALLBACK::_sizetype)-1)
+				if(t == GINVALID)
 					return GAIA::False;
 				m_listCreateCB.erase(t);
 				return GAIA::True;
@@ -41,19 +41,148 @@ namespace GAIA
 				GAIA_AST(!!pCallBack);
 				if(pCallBack == GNIL)
 					return GAIA::False;
-				if(m_listCreateCB.find(pCallBack, 0) == (LIST_CREATECALLBACK::_sizetype)-1)
+				if(m_listCreateCB.find(pCallBack, 0) == GINVALID)
 					return GAIA::False;
 				return GAIA::True;
+			}
+			GINL GAIA::BL BeginRegistClsID()
+			{
+				if(this->IsBeginRegistClsID())
+					return GAIA::False;
+				GAIA_AST(m_PrepareRegClsIDSet.empty());
+				__ClsIDListType::it it = m_RegClsIDList.front_it();
+				for(; !it.empty(); ++it)
+					m_PrepareRegClsIDSet.insert(*it);
+				m_RegClsIDList.clear();
+				m_bBeginRegClsID = GAIA::True;
+				return GAIA::True;
+			}
+			GINL GAIA::BL EndRegistClsID()
+			{
+				if(!this->IsBeginRegistClsID())
+					return GAIA::False;
+				GAIA_AST(m_RegClsIDList.empty());
+				__ClsIDSetType::it it = m_PrepareRegClsIDSet.front_it();
+				for(; !it.empty(); ++it)
+					m_RegClsIDList.push_back(*it);
+				m_PrepareRegClsIDSet.clear();
+				m_bBeginRegClsID = GAIA::False;
+				return GAIA::True;
+			}
+			GINL GAIA::BL IsBeginRegistClsID() const
+			{
+				return m_bBeginRegClsID;
+			}
+			GINL GAIA::BL RegistClsID(const GAIA::FWORK::ClsID& cid)
+			{
+				GAIA_AST(!cid.IsInvalid());
+				if(cid.IsInvalid())
+					return GAIA::False;
+				if(this->IsRegistedClsID(cid))
+					return GAIA::False;
+				CLSIDNODE node;
+				node.cid = cid;
+				if(this->IsBeginRegistClsID())
+				{
+					if(!m_PrepareRegClsIDSet.insert(node))
+					{
+						GAIA_AST(GAIA::False);
+						return GAIA::False;
+					}
+				}
+				else
+				{
+					__ClsIDListType::it it = m_RegClsIDList.lower_bound(node);
+					if(it.empty())
+						m_RegClsIDList.push_back(node);
+					else
+					{
+						if(!m_RegClsIDList.insert(node, it - m_RegClsIDList.front_it()))
+						{
+							GAIA_AST(GAIA::False);
+							return GAIA::False;
+						}
+					}
+				}
+				return GAIA::True;
+			}
+			GINL GAIA::BL UnregistClsID(const GAIA::FWORK::ClsID& cid)
+			{
+				GAIA_AST(!cid.IsInvalid());
+				if(cid.IsInvalid())
+					return GAIA::False;
+				if(!this->IsRegistedClsID(cid))
+					return GAIA::False;
+				CLSIDNODE node;
+				node.cid = cid;
+				if(this->IsBeginRegistClsID())
+				{
+					if(!m_PrepareRegClsIDSet.erase(node))
+					{
+						GAIA_AST(GAIA::False);
+						return GAIA::False;
+					}
+				}
+				else
+				{
+					__ClsIDListType::_sizetype index = m_RegClsIDList.search(node);
+					GAIA_AST(index != GINVALID);
+					if(!m_RegClsIDList.erase(index))
+					{
+						GAIA_AST(GAIA::False);
+						return GAIA::False;
+					}
+				}
+				return GAIA::True;
+			}
+			GINL GAIA::BL IsRegistedClsID(const GAIA::FWORK::ClsID& cid) const
+			{
+				CLSIDNODE finder;
+				finder.cid = cid;
+				if(this->IsBeginRegistClsID())
+				{
+					if(m_PrepareRegClsIDSet.find(finder) != GNIL)
+						return GAIA::True;
+				}
+				else
+				{
+					if(m_RegClsIDList.search(finder) != GINVALID)
+						return GAIA::True;
+				}
+				return GAIA::False;
 			}
 			GINL GAIA::GVOID CollectCreateCallBack(GAIA::CTN::Vector<FactoryCreateCallBack*>& listResult) const{listResult = m_listCreateCB;}
 			GINL virtual Instance* CreateInstance(const ClsID& cid, GAIA::GVOID* pParameter);
 			GINL virtual Instance* RequestInstance(const ClsID& cid, GAIA::GVOID* pParameter);
+		public:
+			class CLSIDNODE
+			{
+			public:
+				GINL CLSIDNODE& operator = (const CLSIDNODE& src)
+				{
+					cid = src.cid;
+					return *this;
+				}
+				GAIA_CLASS_OPERATOR_COMPARE(cid, cid, CLSIDNODE);
+				GAIA::FWORK::ClsID cid;
+			};
 		private:
+			GINL GAIA::GVOID init()
+			{
+				m_bBeginRegClsID = GAIA::False;
+			}
 			GINL virtual GAIA::GVOID CacheInstance(Instance* pInstance)
 			{
+				GAIA_AST(pInstance != GNIL);
 			}
 		private:
+			typedef GAIA::CTN::Vector<CLSIDNODE> __ClsIDListType;
+			typedef GAIA::CTN::Set<CLSIDNODE> __ClsIDSetType;
+		private:
 			LIST_CREATECALLBACK m_listCreateCB;
+			__ClsIDListType m_RegClsIDList;
+			__ClsIDSetType m_PrepareRegClsIDSet;
+			GAIA::BL m_bBeginRegClsID;
 		};
 	};
 };
