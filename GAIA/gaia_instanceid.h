21a2262cb295ebb574496f4a9e35e62bcbc18dcd
Armterla <70601797@qq.com>
2014-10-06 10:53:39 +0800

    goon

--- GAIA/gaia_instanceid.h
+++ GAIA/gaia_instanceid.h
@@ -1,13 +1,19 @@
 #ifndef		__GAIA_INSTANCE_ID_H__
 #define		__GAIA_INSTANCE_ID_H__
 
-class InstanceId : public virtual GAIA::Base
+namespace GAIA
 {
-public:
-	GINL InstanceId(){m_rid.uuid();}
-	GINL const GAIA::MATH::RID128& GetInstanceID() const{return m_rid;}
-private:
-	GAIA::MATH::RID128 m_rid;
+	namespace FWORK
+	{
+		class InstanceId : public virtual GAIA::Base
+		{
+		public:
+			GINL InstanceId(){m_rid.uuid();}
+			GINL const GAIA::MATH::RID128& GetInstanceID() const{return m_rid;}
+		private:
+			GAIA::MATH::RID128 m_rid;
+		};
+	};
 };
 
 #endif
