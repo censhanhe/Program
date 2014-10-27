da4b03a3ce9a87ec5271ab5c1c6941195618bc3e
Armterla <70601797@qq.com>
2014-10-27 15:41:24 +0800

    goon

--- GAIA/gaia_math_mtx22.h
+++ GAIA/gaia_math_mtx22.h
@@ -8,6 +8,81 @@ namespace GAIA
 		template<typename _DataType> class MTX22 : public GAIA::Base
 		{
 		public:
+			typedef _DataType _datatype;
+		public:
+			typedef MTX22<_DataType> __MyType;
+		public:
+			GINL MTX22(){}
+			GINL MTX22(const __MyType& src){this->operator = (src);}
+			template<typename _ParamDataType> MTX22(const _ParamDataType* p){this->operator = (p);}
+			template<typename _ParamDataType> MTX22(_ParamDataType* p){this->operator = (p);}
+			template<typename _ParamDataType> MTX22(const _ParamDataType& t){this->operator = (t);}
+			GINL GAIA::SIZE size() const{return sizeofarray2(m);}
+			GINL GAIA::SIZE sizex() const{return sizeofarray(m[0]);}
+			GINL GAIA::SIZE sizey() const{return this->size() / this->sizex();}
+			GINL GAIA::GVOID identity()
+			{
+				m[0][0] = (_DataType)1; m[0][1] = (_DataType)0;
+				m[1][0] = (_DataType)0; m[1][1] = (_DataType)1;
+			}
+			GINL GAIA::BL isidentity() const
+			{
+				if(	m[0][0] == (_DataType)1 && m[0][1] == (_DataType)0 && 
+					m[1][0] == (_DataType)0 && m[1][1] == (_DataType)1)
+					return GAIA::True;
+				return GAIA::False;
+			}
+			GINL GAIA::BL isalmostidentity() const
+			{
+				if(	GAIA::MATH::xequal(m[0][0], (_DataType)1) && GAIA::MATH::xequal(m[0][1], (_DataType)0) && 
+					GAIA::MATH::xequal(m[1][0], (_DataType)0) && GAIA::MATH::xequal(m[1][1], (_DataType)1))
+					return GAIA::True;
+				return GAIA::False;
+			}
+			GINL GAIA::BL isalmostidentity(const _DataType& epsilon) const
+			{
+				if(	GAIA::MATH::xequal(m[0][0], (_DataType)1, epsilon) && GAIA::MATH::xequal(m[0][1], (_DataType)0, epsilon) && 
+					GAIA::MATH::xequal(m[1][0], (_DataType)0, epsilon) && GAIA::MATH::xequal(m[1][1], (_DataType)1, epsilon))
+					return GAIA::True;
+				return GAIA::False;
+			}
+			GINL GAIA::BL isfinited() const
+			{
+				return 
+					GAIA::MATH::xfinite(m[0][0]) && 
+					GAIA::MATH::xfinite(m[0][1]) && 
+					GAIA::MATH::xfinite(m[1][0]) && 
+					GAIA::MATH::xfinite(m[1][1]));
+			}
+			GINL const _DataType* front_ptr() const{return GRCAST(const _DataType*)(m);}
+			GINL _DataType* front_ptr(){return GRCAST(_DataType*)(m);}
+			GINL const _DataType* back_ptr() const{return this->front_ptr() + this->size() - 1;}
+			GINL _DataType* back_ptr(){return this->front_ptr() + this->size() - 1;}
+			GINL __MyType& operator = (const __MyType& src)
+			{
+				m[0][0] = (_DataType)src.m[0][0]; m[0][1] = (_DataType)src.m[0][1];
+				m[1][0] = (_DataType)src.m[1][0]; m[1][1] = (_DataType)src.m[1][1];
+				return *this;
+			}
+			template<typename _ParamDataType> __MyType& operator = (const _ParamDataType* p){GAIA_AST(p != GNIL); GAIA::ALGO::copy(this->front_ptr(), p, this->size()); return *this;}
+			template<typename _ParamDataType> __MyType& operator = (_ParamDataType* p){return this->operator = (GSCAST(const _ParamDataType*)(p));}
+			template<typename _ParamDataType> __MyType& operator = (const _ParamDataType& t){GAIA::ALGO::set(this->front_ptr(), t, this->size()); return *this;}
+			template<typename _ParamDataType> GAIA::BL operator == (const GAIA::MATH::MTX22<_ParamDataType>& src) const{return GAIA::ALGO::cmps(this->front_ptr(), src.front_ptr(), this->size()) == 0;}
+			template<typename _ParamDataType> GAIA::BL operator != (const GAIA::MATH::MTX22<_ParamDataType>& src) const{return !(this->operator == (src));}
+			template<typename _ParamDataType> GAIA::BL operator >= (const GAIA::MATH::MTX22<_ParamDataType>& src) const{return GAIA::ALGO::cmps(this->front_ptr(), src.front_ptr(), this->size()) >= 0;}
+			template<typename _ParamDataType> GAIA::BL operator <= (const GAIA::MATH::MTX22<_ParamDataType>& src) const{return GAIA::ALGO::cmps(this->front_ptr(), src.front_ptr(), this->size()) <= 0;}
+			template<typename _ParamDataType> GAIA::BL operator > (const GAIA::MATH::MTX22<_ParamDataType>& src) const{return !(this->operator <= (src));}
+			template<typename _ParamDataType> GAIA::BL operator < (const GAIA::MATH::MTX22<_ParamDataType>& src) const{return !(this->operator >= (src));}
+			template<typename _ParamDataType> GAIA::BL operator == (const _ParamDataType* p) const{return GAIA::ALGO::cmps(this->front_ptr(), p, this->size()) == 0;}
+			template<typename _ParamDataType> GAIA::BL operator != (const _ParamDataType* p) const{return GAIA::ALGO::cmps(this->front_ptr(), p, this->size()) != 0;}
+			template<typename _ParamDataType> GAIA::BL operator >= (const _ParamDataType* p) const{return GAIA::ALGO::cmps(this->front_ptr(), p, this->size()) >= 0;}
+			template<typename _ParamDataType> GAIA::BL operator <= (const _ParamDataType* p) const{return GAIA::ALGO::cmps(this->front_ptr(), p, this->size()) <= 0;}
+			template<typename _ParamDataType> GAIA::BL operator > (const _ParamDataType* p) const{return GAIA::ALGO::cmps(this->front_ptr(), p, this->size()) > 0;}
+			template<typename _ParamDataType> GAIA::BL operator < (const _ParamDataType* p) const{return GAIA::ALGO::cmps(this->front_ptr(), p, this->size()) < 0;}
+			template<typename _ParamDataType> const _DataType& operator [] (const _ParamDataType& index) const{GAIA_AST(index >= 0 && index < this->size()); return this->front_ptr()[index];}
+			template<typename _ParamDataType> _DataType& operator [] (const _ParamDataType& index){GAIA_AST(index >= 0 && index < this->size()); return this->front_ptr()[index];}
+			template<typename _ParamDataType1, typename _ParamDataType2> _DataType& operator () (const _ParamDataType1& i1, const _ParamDataType2& i2){GAIA_AST(i1 >= 0 && i1 < 3); GAIA_AST(i2 >= 0 && i2 < 3); return m[i1][i2];}
+		public:
 			_DataType m[2][2];
 		};
 	};
