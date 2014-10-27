93ab7d6f9a8431ea49abe8825d69f3418597e89f
Armterla <70601797@qq.com>
2014-10-27 15:30:48 +0800

    goon

--- GAIA/gaia_math_mtx43.h
+++ GAIA/gaia_math_mtx43.h
@@ -70,66 +70,6 @@ namespace GAIA
 					GAIA::MATH::xfinite(m[3][1]) && 
 					GAIA::MATH::xfinite(m[3][2]);
 			}
-
-			GINL _DataType inverse()
-			{
-			}
-			template<typename _ParamDataType1, typename _ParamDataType2, typename _ParamDataType3> GAIA::GVOID translate(const _ParamDataType1& x, const _ParamDataType2& y, const _ParamDataType3& z)
-			{
-				m[0][0] = (_DataType)1; m[0][1] = (_DataType)0; m[0][2] = (_DataType)0;
-				m[1][0] = (_DataType)0; m[1][1] = (_DataType)1; m[1][2] = (_DataType)0;
-				m[2][0] = (_DataType)0; m[2][1] = (_DataType)0; m[2][2] = (_DataType)1;
-				m[3][0] = x;			m[3][1] = y;			m[3][2] = z;
-			}
-			template<typename _ParamDataType> GAIA::GVOID rotatex(const _ParamDataType& x)
-			{
-				_DataType cosv = GAIA::MATH::xcos(x);
-				_DataType sinv = GAIA::MATH::xsin(x);
-				m[0][0] = (_DataType)1; m[0][1] = (_DataType)0; m[0][2] = (_DataType)0;
-				m[1][0] = (_DataType)0; m[1][1] = cosv;			m[1][2] = sinv;
-				m[2][0] = (_DataType)0; m[2][1] = -sinv;		m[2][2] = cosv;
-				m[3][0] = (_DataType)0; m[3][1] = (_DataType)0; m[3][2] = (_DataType)0;
-			}
-			template<typename _ParamDataType> GAIA::GVOID rotatey(const _ParamDataType& y)
-			{
-				_DataType cosv = GAIA::MATH::xcos(y);
-				_DataType sinv = GAIA::MATH::xsin(y);
-				m[0][0] = cosv;			m[0][1] = (_DataType)0; m[0][2] = -sinv;
-				m[1][0] = (_DataType)0; m[1][1] = (_DataType)1; m[1][2] = (_DataType)0;
-				m[2][0] = sinv;			m[2][1] = (_DataType)0; m[2][2] = cosv;
-				m[3][0] = (_DataType)0; m[3][1] = (_DataType)0; m[3][2] = (_DataType)0;
-			}
-			template<typename _ParamDataType> GAIA::GVOID rotatez(const _ParamDataType& z)
-			{
-				_DataType cosv = GAIA::MATH::xcos(z);
-				_DataType sinv = GAIA::MATH::xsin(z);
-				m[0][0] = cosv;			m[0][1] = sinv;			m[0][2] = (_DataType)0;
-				m[1][0] = -sinv;		m[1][1] = cosv;			m[1][2] = (_DataType)0;
-				m[2][0] = (_DataType)0; m[2][1] = (_DataType)0; m[2][2] = (_DataType)1;
-				m[3][0] = (_DataType)0; m[3][1] = (_DataType)0; m[3][2] = (_DataType)0;
-			}
-			template<typename _ParamDataType1, typename _ParamDataType2> GAIA::GVOID rotate(const GAIA::MATH::VEC3<_ParamDataType1>& nor, const _ParamDataType2& radian)
-			{
-				_DataType cosv = GAIA::MATH::xcos(radian);
-				_DataType sinv = GAIA::MATH::xsin(radian);
-				_DataType cosvi = (_DataType)1 - cosv;
-				m[0][0] = nor.x * nor.x * cosvi + cosv;			m[0][1] = nor.x * nor.y * cosvi + nor.z * sinv; m[0][2] = nor.x * nor.z * cosvi - nor.y * sinv;
-				m[1][0] = nor.x * nor.y * cosvi - nor.z * sinv; m[1][1] = nor.y * nor.y * cosvi + cosv;			m[1][2] = nor.y * nor.z * cosvi + nor.x * sinv;
-				m[2][0] = nor.x * nor.z * cosvi + nor.y * sinv; m[2][1] = nor.y * nor.z * cosvi - nor.x * sinv; m[2][2] = nor.z * nor.z * cosvi + cosv;
-				m[3][0] = (_DataType)0;							m[3][1] = (_DataType)0;							m[3][2] = (_DataType)0;
-			}
-			template<typename _ParamDataType1, typename _ParamDataType2, typename _ParamDataType3> GAIA::GVOID scale(const _ParamDataType1& x, const _ParamDataType2& y, const _ParamDataType3& z)
-			{
-				m[0][0] =    x;			m[0][1] = (_DataType)0; m[0][2] = (_DataType)0;
-				m[1][0] = (_DataType)0; m[1][1] =    y;			m[1][2] = (_DataType)0;
-				m[2][0] = (_DataType)0; m[2][1] = (_DataType)0; m[2][2] =    z;
-				m[3][0] = (_DataType)0; m[3][1] = (_DataType)0; m[3][2] = (_DataType)0;
-			}
-			template<typename _ParamDataType> GAIA::GVOID position(const GAIA::MATH::VEC3<_ParamDataType>& src){m[3][0] = src.x; m[3][1] = src.y; m[3][2] = src.z;}
-			GAIA::MATH::VEC3<_DataType> position() const{GAIA::MATH::VEC3<_DataType> ret; ret.x = m[3][0]; ret.y = m[3][1]; ret.z = m[3][2]; return ret;}
-			template<typename _ParamEndDataType, typename _ParamFactorDataType> __MyType& lerp(const GAIA::MATH::MTX43<_ParamEndDataType>& end, const _ParamFactorDataType& factor)
-			{
-			}
 			GINL const _DataType* front_ptr() const{return GRCAST(const _DataType*)(m);}
 			GINL _DataType* front_ptr(){return GRCAST(_DataType*)(m);}
 			GINL const _DataType* back_ptr() const{return this->front_ptr() + this->size() - 1;}
