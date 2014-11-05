95859110626e6ca2e52915cb16d473d656515471
Armterla <70601797@qq.com>
2014-11-05 16:39:20 +0800

    goon

--- GAIA/TEST/t_container_vari.cpp
+++ GAIA/TEST/t_container_vari.cpp
@@ -36,6 +36,16 @@ namespace GAIA_TEST
 			GTLINE2("Vari operator = error!");
 			++nRet;
 		}
+		if(v1.type() != GAIA::TYPEID_NM || v2.type() != GAIA::TYPEID_NM)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::NM)v1 != 1 || (GAIA::NM)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
+			++nRet;
+		}
 
 		/* UM. */
 		v1 = GSCAST(GAIA::UM)(1);
@@ -48,6 +58,16 @@ namespace GAIA_TEST
 		if(v2.type() != GAIA::TYPEID_UM || !v2.check())
 		{
 			GTLINE2("Vari operator = error!");
+			++nRet;
+		}
+		if(v1.type() != GAIA::TYPEID_UM || v2.type() != GAIA::TYPEID_UM)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::UM)v1 != 1 || (GAIA::UM)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
 			++nRet;
 		}
 
@@ -64,6 +84,16 @@ namespace GAIA_TEST
 			GTLINE2("Vari operator = error!");
 			++nRet;
 		}
+		if(v1.type() != GAIA::TYPEID_BL || v2.type() != GAIA::TYPEID_BL)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::BL)v1 != GAIA::True || (GAIA::BL)v2 != GAIA::True)
+		{
+			GTLINE2("Vari convert to value error!");
+			++nRet;
+		}
 
 		/* N8. */
 		v1 = GSCAST(GAIA::N8)(1);
@@ -76,6 +106,16 @@ namespace GAIA_TEST
 		if(v2.type() != GAIA::TYPEID_N8 || !v2.check())
 		{
 			GTLINE2("Vari operator = error!");
+			++nRet;
+		}
+		if(v1.type() != GAIA::TYPEID_N8 || v2.type() != GAIA::TYPEID_N8)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::N8)v1 != 1 || (GAIA::N8)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
 			++nRet;
 		}
 
@@ -92,6 +132,16 @@ namespace GAIA_TEST
 			GTLINE2("Vari operator = error!");
 			++nRet;
 		}
+		if(v1.type() != GAIA::TYPEID_N16 || v2.type() != GAIA::TYPEID_N16)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::N16)v1 != 1 || (GAIA::N16)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
+			++nRet;
+		}
 
 		/* N32. */
 		v1 = GSCAST(GAIA::N32)(1);
@@ -104,6 +154,16 @@ namespace GAIA_TEST
 		if(v2.type() != GAIA::TYPEID_N32 || !v2.check())
 		{
 			GTLINE2("Vari operator = error!");
+			++nRet;
+		}
+		if(v1.type() != GAIA::TYPEID_N32 || v2.type() != GAIA::TYPEID_N32)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::N32)v1 != 1 || (GAIA::N32)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
 			++nRet;
 		}
 
@@ -120,6 +180,16 @@ namespace GAIA_TEST
 			GTLINE2("Vari operator = error!");
 			++nRet;
 		}
+		if(v1.type() != GAIA::TYPEID_N64 || v2.type() != GAIA::TYPEID_N64)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::N64)v1 != 1 || (GAIA::N64)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
+			++nRet;
+		}
 
 		/* U8. */
 		v1 = GSCAST(GAIA::U8)(1);
@@ -132,6 +202,16 @@ namespace GAIA_TEST
 		if(v2.type() != GAIA::TYPEID_U8 || !v2.check())
 		{
 			GTLINE2("Vari operator = error!");
+			++nRet;
+		}
+		if(v1.type() != GAIA::TYPEID_U8 || v2.type() != GAIA::TYPEID_U8)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::U8)v1 != 1 || (GAIA::U8)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
 			++nRet;
 		}
 
@@ -148,6 +228,16 @@ namespace GAIA_TEST
 			GTLINE2("Vari operator = error!");
 			++nRet;
 		}
+		if(v1.type() != GAIA::TYPEID_U16 || v2.type() != GAIA::TYPEID_U16)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::U16)v1 != 1 || (GAIA::U16)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
+			++nRet;
+		}
 
 		/* U32. */
 		v1 = GSCAST(GAIA::U32)(1);
@@ -160,6 +250,16 @@ namespace GAIA_TEST
 		if(v2.type() != GAIA::TYPEID_U32 || !v2.check())
 		{
 			GTLINE2("Vari operator = error!");
+			++nRet;
+		}
+		if(v1.type() != GAIA::TYPEID_U32 || v2.type() != GAIA::TYPEID_U32)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::U32)v1 != 1 || (GAIA::U32)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
 			++nRet;
 		}
 
@@ -176,6 +276,16 @@ namespace GAIA_TEST
 			GTLINE2("Vari operator = error!");
 			++nRet;
 		}
+		if(v1.type() != GAIA::TYPEID_U64 || v2.type() != GAIA::TYPEID_U64)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::U64)v1 != 1 || (GAIA::U64)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
+			++nRet;
+		}
 
 		/* F32. */
 		v1 = GSCAST(GAIA::F32)(1);
@@ -190,6 +300,16 @@ namespace GAIA_TEST
 			GTLINE2("Vari operator = error!");
 			++nRet;
 		}
+		if(v1.type() != GAIA::TYPEID_F32 || v2.type() != GAIA::TYPEID_F32)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::F32)v1 != 1 || (GAIA::F32)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
+			++nRet;
+		}
 
 		/* F64. */
 		v1 = GSCAST(GAIA::F64)(1);
@@ -202,6 +322,16 @@ namespace GAIA_TEST
 		if(v2.type() != GAIA::TYPEID_F64 || !v2.check())
 		{
 			GTLINE2("Vari operator = error!");
+			++nRet;
+		}
+		if(v1.type() != GAIA::TYPEID_F64 || v2.type() != GAIA::TYPEID_F64)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::F64)v1 != 1 || (GAIA::F64)v2 != 1)
+		{
+			GTLINE2("Vari convert to value error!");
 			++nRet;
 		}
 
@@ -219,6 +349,16 @@ namespace GAIA_TEST
 			GTLINE2("Vari operator = error!");
 			++nRet;
 		}
+		if(v1.type() != GAIA::TYPEID_POINTER || v2.type() != GAIA::TYPEID_POINTER)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((GAIA::GVOID*)v1 != GNIL || (GAIA::GVOID*)v2 != GNIL)
+		{
+			GTLINE2("Vari convert to value error!");
+			++nRet;
+		}
 
 		/* ConstPointer. */
 		const GAIA::GVOID* cp = GNIL;
@@ -232,6 +372,16 @@ namespace GAIA_TEST
 		if(v2.type() != GAIA::TYPEID_CONSTPOINTER || !v2.check())
 		{
 			GTLINE2("Vari operator = error!");
+			++nRet;
+		}
+		if(v1.type() != GAIA::TYPEID_CONSTPOINTER || v2.type() != GAIA::TYPEID_CONSTPOINTER)
+		{
+			GTLINE2("Vari type error!");
+			++nRet;
+		}
+		if((const GAIA::GVOID*)v1 != GNIL || (const GAIA::GVOID*)v2 != GNIL)
+		{
+			GTLINE2("Vari convert to value error!");
 			++nRet;
 		}
 
