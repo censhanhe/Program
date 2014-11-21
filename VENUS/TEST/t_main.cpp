c18389505da66323e827039bf14ea0825b1e0fd5
Armterla <70601797@qq.com>
2014-11-21 19:31:43 +0800

    venus render impl

--- VENUS/TEST/t_main.cpp
+++ VENUS/TEST/t_main.cpp
@@ -1,43 +1,33 @@
 #include "../venus.h"
 #include "gaia_global_impl.h"
 
-class ScreenVertex
-{
-public:
-	GAIA::MATH::VEC2<GAIA::F32> pos;
-	GAIA::MATH::ARGB<GAIA::F32> cr;
-	GAIA::MATH::VEC2<GAIA::F32> tc;
-};
-
 class Vertex
 {
 public:
+	GAIA::MATH::VEC3<GAIA::F32> pos;
+	GAIA::MATH::VEC3<GAIA::F32> nor;
 };
 
 static const GAIA::CH VERTEXSHADERSTRING[] = 
 "\
-attribute vec2 a_pos;\
-attribute vec4 a_cr;\
-attribute vec2 a_tc;\
-varying vec4 v_cr;\
-varying vec2 v_tc;\
+attribute vec3 a_pos;\
+attribute vec3 a_nor;\
+varying vec3 v_nor;\
+uniform mat4 u_mtx;\
 void main()\
 {\
-	gl_Position = vec4(a_pos.x, a_pos.y, 0.5, 1.0);\
-	v_cr = a_cr;\
-	v_tc = a_tc;\
+	gl_Position = u_mtx * vec4(a_pos.x, a_pos.y, a_pos.z, 1);\
+	v_nor = (u_mtx * vec4(a_nor.x, a_nor.y, a_nor.z, 0)).xyz;\
 }\
 ";
 
 static const GAIA::CH PIXELSHADERSTRING[] = 
 "\
-varying vec4 v_cr;\
-varying vec2 v_tc;\
-uniform float u_time;\
-uniform sampler2D u_tex;\
+varying vec3 v_nor;\
 void main()\
 {\
-	gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0) * v_cr * abs(sin(u_time)) * texture2D(u_tex, v_tc);\
+	gl_FragColor.xyz = v_nor;\
+	gl_FragColor.w = 1;\
 }\
 ";
 
@@ -49,49 +39,129 @@ public:
 	GAIA::BL Create(VENUS::Render& r, VENUS::Render::Context& ctx)
 	{
 		{
+			// Create vertex buffer.
 			VENUS::Render::VertexBuffer::Desc descVB;
 			descVB.reset();
 			descVB.sElementCount = 3;
 			descVB.sElementSize = sizeof(GAIA::MATH::VEC2<GAIA::F32>);
-			pScreenTriangleVB = r.CreateVertexBuffer(descVB);
-			ScreenVertex v[3];
-			v[0].pos = GAIA::MATH::VEC2<GAIA::F32>(-0.5F, -0.5F);
-			v[1].pos = GAIA::MATH::VEC2<GAIA::F32>(+0.5F, -0.5F);
-			v[2].pos = GAIA::MATH::VEC2<GAIA::F32>(0.0F, 0.5F);
-			v[0].cr = GAIA::MATH::ARGB<GAIA::F32>(1.0F, 1.0F, 0.0F, 0.0F);
-			v[1].cr = GAIA::MATH::ARGB<GAIA::F32>(1.0F, 0.0F, 1.0F, 0.0F);
-			v[2].cr = GAIA::MATH::ARGB<GAIA::F32>(1.0F, 0.0F, 0.0F, 1.0F);
-			v[0].tc = GAIA::MATH::VEC2<GAIA::F32>(-1.0F, -1.0F);
-			v[1].tc = GAIA::MATH::VEC2<GAIA::F32>(-1.0F, 2.0F);
-			v[2].tc = GAIA::MATH::VEC2<GAIA::F32>(2.0F, 2.0F);
-			pScreenTriangleVB->Commit(0, sizeof(v), v);
+			pTriangleVB = r.CreateVertexBuffer(descVB);
+			Vertex v[6 * 4];
 
+			GAIA::MATH::MTX44<GAIA::REAL> mtxR, mtx[6];
+			mtx[0].identity();
+			mtxR.rotatey(GAIA::MATH::PI * R(+0.5));
+			mtx[1] = mtxR;
+			mtxR.rotatey(GAIA::MATH::PI * R(-0.5));
+			mtx[2] = mtxR;
+			mtxR.rotatex(GAIA::MATH::PI * R(+0.5));
+			mtx[3] = mtxR;
+			mtxR.rotatex(GAIA::MATH::PI * R(-0.5));
+			mtx[4] = mtxR;
+			mtxR.rotatex(GAIA::MATH::PI);
+			mtx[5] = mtxR;
+
+			v[4 * 0 + 0].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, -1.0F, +1.0F) * mtx[0];
+			v[4 * 0 + 1].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, -1.0F, +1.0F) * mtx[0];
+			v[4 * 0 + 2].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, +1.0F, +1.0F) * mtx[0];
+			v[4 * 0 + 3].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, +1.0F, +1.0F) * mtx[0];
+
+			v[4 * 1 + 0].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, -1.0F, +1.0F) * mtx[1];
+			v[4 * 1 + 1].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, -1.0F, +1.0F) * mtx[1];
+			v[4 * 1 + 2].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, +1.0F, +1.0F) * mtx[1];
+			v[4 * 1 + 3].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, +1.0F, +1.0F) * mtx[1];
+
+			v[4 * 2 + 0].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, -1.0F, +1.0F) * mtx[2];
+			v[4 * 2 + 1].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, -1.0F, +1.0F) * mtx[2];
+			v[4 * 2 + 2].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, +1.0F, +1.0F) * mtx[2];
+			v[4 * 2 + 3].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, +1.0F, +1.0F) * mtx[2];
+
+			v[4 * 3 + 0].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, -1.0F, +1.0F) * mtx[3];
+			v[4 * 3 + 1].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, -1.0F, +1.0F) * mtx[3];
+			v[4 * 3 + 2].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, +1.0F, +1.0F) * mtx[3];
+			v[4 * 3 + 3].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, +1.0F, +1.0F) * mtx[3];
+
+			v[4 * 4 + 0].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, -1.0F, +1.0F) * mtx[4];
+			v[4 * 4 + 1].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, -1.0F, +1.0F) * mtx[4];
+			v[4 * 4 + 2].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, +1.0F, +1.0F) * mtx[4];
+			v[4 * 4 + 3].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, +1.0F, +1.0F) * mtx[4];
+
+			v[4 * 5 + 0].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, -1.0F, +1.0F) * mtx[5];
+			v[4 * 5 + 1].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, -1.0F, +1.0F) * mtx[5];
+			v[4 * 5 + 2].pos = GAIA::MATH::VEC3<GAIA::REAL>(+1.0F, +1.0F, +1.0F) * mtx[5];
+			v[4 * 5 + 3].pos = GAIA::MATH::VEC3<GAIA::REAL>(-1.0F, +1.0F, +1.0F) * mtx[5];
+
+			v[4 * 0 + 0].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[0];
+			v[4 * 0 + 1].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[0];
+			v[4 * 0 + 2].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[0];
+			v[4 * 0 + 3].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[0];
+
+			v[4 * 1 + 0].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[1];
+			v[4 * 1 + 1].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[1];
+			v[4 * 1 + 2].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[1];
+			v[4 * 1 + 3].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[1];
+
+			v[4 * 2 + 0].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[2];
+			v[4 * 2 + 1].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[2];
+			v[4 * 2 + 2].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[2];
+			v[4 * 2 + 3].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[2];
+
+			v[4 * 3 + 0].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[3];
+			v[4 * 3 + 1].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[3];
+			v[4 * 3 + 2].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[3];
+			v[4 * 3 + 3].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[3];
+
+			v[4 * 4 + 0].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[4];
+			v[4 * 4 + 1].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[4];
+			v[4 * 4 + 2].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[4];
+			v[4 * 4 + 3].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[4];
+
+			v[4 * 5 + 0].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[5];
+			v[4 * 5 + 1].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[5];
+			v[4 * 5 + 2].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[5];
+			v[4 * 5 + 3].nor = GAIA::MATH::VEC3<GAIA::REAL>(+0.0F, +0.0F, +1.0F) * mtx[5];
+
+			pTriangleVB->Commit(0, sizeof(v), v);
+
+			// Create index buffer.
 			VENUS::Render::IndexBuffer::Desc descIB;
 			descIB.reset();
 			descIB.fmt = VENUS::Render::FORMAT_INDEX16;
 			descIB.sCount = 3;
-			pScreenTriangleIB = r.CreateIndexBuffer(descIB);
-			GAIA::U16 i[3] = {0, 1, 2};
-			pScreenTriangleIB->Commit(0, sizeof(i), i);
+			pTriangleIB = r.CreateIndexBuffer(descIB);
+			GAIA::U16 i[6 * 2 * 3] = {0, 1, 2};
+			for(GAIA::SIZE x = 0; x < 6; ++x)
+			{
+				i[x * 6 + 0] = 0 + x * 4;
+				i[x * 6 + 1] = 1 + x * 4;
+				i[x * 6 + 2] = 2 + x * 4;
 
+				i[x * 6 + 3] = 0 + x * 4;
+				i[x * 6 + 4] = 2 + x * 4;
+				i[x * 6 + 5] = 3 + x * 4;
+			}
+			pTriangleIB->Commit(0, sizeof(i), i);
+
+			// Create shader.
 			VENUS::Render::Shader::Desc descVShader;
 			descVShader.reset();
 			descVShader.type = VENUS::Render::SHADER_TYPE_VERTEXSHADER;
-			pScreenVShader = r.CreateShader(descVShader);
-			pScreenVShader->Commit(VERTEXSHADERSTRING);
+			pVShader = r.CreateShader(descVShader);
+			pVShader->Commit(VERTEXSHADERSTRING);
 
 			VENUS::Render::Shader::Desc descPShader;
 			descPShader.reset();
 			descPShader.type = VENUS::Render::SHADER_TYPE_PIXELSHADER;
-			pScreenPShader = r.CreateShader(descPShader);
-			pScreenPShader->Commit(PIXELSHADERSTRING);
+			pPShader = r.CreateShader(descPShader);
+			pPShader->Commit(PIXELSHADERSTRING);
 
 			VENUS::Render::Program::Desc descProgram;
 			descProgram.reset();
-			descProgram.pVS = pScreenVShader;
-			descProgram.pPS = pScreenPShader;
-			pScreenProgram = r.CreateProgram(descProgram);
+			descProgram.pVS = pVShader;
+			descProgram.pPS = pPShader;
+			pProgram = r.CreateProgram(descProgram);
+			GAIA_AST(pProgram != GNIL);
 
+			// Create texture.
 			VENUS::Render::Texture::Desc descTex;
 			descTex.reset();
 			descTex.type = VENUS::Render::TEXTURE_TYPE_2D;
@@ -123,29 +193,29 @@ public:
 	}
 	GAIA::BL Destroy()
 	{
-		GAIA_RELEASE_SAFE(pScreenTriangleVB);
-		GAIA_RELEASE_SAFE(pScreenTriangleIB);
-		GAIA_RELEASE_SAFE(pScreenVShader);
-		GAIA_RELEASE_SAFE(pScreenPShader);
-		GAIA_RELEASE_SAFE(pScreenProgram);
+		GAIA_RELEASE_SAFE(pTriangleVB);
+		GAIA_RELEASE_SAFE(pTriangleIB);
+		GAIA_RELEASE_SAFE(pVShader);
+		GAIA_RELEASE_SAFE(pPShader);
+		GAIA_RELEASE_SAFE(pProgram);
 		GAIA_RELEASE_SAFE(pTex);
 		return GAIA::True;
 	}
 public:
-	VENUS::Render::VertexBuffer* pScreenTriangleVB;
-	VENUS::Render::IndexBuffer* pScreenTriangleIB;
-	VENUS::Render::Shader* pScreenVShader;
-	VENUS::Render::Shader* pScreenPShader;
-	VENUS::Render::Program* pScreenProgram;
+	VENUS::Render::VertexBuffer* pTriangleVB;
+	VENUS::Render::IndexBuffer* pTriangleIB;
+	VENUS::Render::Shader* pVShader;
+	VENUS::Render::Shader* pPShader;
+	VENUS::Render::Program* pProgram;
 	VENUS::Render::Texture* pTex;
 private:
 	GAIA::GVOID init()
 	{
-		pScreenTriangleVB = GNIL;
-		pScreenTriangleIB = GNIL;
-		pScreenVShader = GNIL;
-		pScreenPShader = GNIL;
-		pScreenProgram = GNIL;
+		pTriangleVB = GNIL;
+		pTriangleIB = GNIL;
+		pVShader = GNIL;
+		pPShader = GNIL;
+		pProgram = GNIL;
 		pTex = GNIL;
 	}
 };
@@ -157,18 +227,30 @@ static GAIA::BL FrameLoop(VENUS::Render::Context& ctx, VENUS::Render& r, Resourc
 	crClear.fromu32(0xFF8080FF);
 	crClear.torealmode();
 	r.ClearTarget(ctx, 0, crClear);
+	r.ClearDepther(ctx, R(1.0));
 
-	// Draw screen triangle.
+	r.SetRenderState(ctx, VENUS::Render::RENDER_STATE_ZTEST, GAIA::True);
+
+	// Draw box.
 	r.SetTexture(ctx, "u_tex", res.pTex);
-	r.SetVertexBuffer(ctx, "a_pos", res.pScreenTriangleVB, sizeof(ScreenVertex), 0);
-	r.SetVertexBuffer(ctx, "a_cr", res.pScreenTriangleVB, sizeof(ScreenVertex), sizeof(GAIA::MATH::VEC2<GAIA::F32>));
-	r.SetVertexBuffer(ctx, "a_tc", res.pScreenTriangleVB, sizeof(ScreenVertex), sizeof(GAIA::MATH::VEC2<GAIA::F32>) + sizeof(GAIA::MATH::ARGB<GAIA::F32>));
-	r.SetIndexBuffer(ctx, res.pScreenTriangleIB);
-	r.SetProgram(ctx, res.pScreenProgram);
-	GAIA::F32 fTime = (GAIA::F32)GAIA::TIME::tick_time() / 1000.0F / 1000.0F;
-	r.SetConstant(ctx, "u_time", &fTime, 1, 1, 1);
+	r.SetVertexBuffer(ctx, "a_pos", res.pTriangleVB, sizeof(Vertex), 0);
+	r.SetVertexBuffer(ctx, "a_nor", res.pTriangleVB, sizeof(Vertex), sizeof(GAIA::MATH::VEC3<GAIA::F32>));
+	r.SetIndexBuffer(ctx, res.pTriangleIB);
+	r.SetProgram(ctx, res.pProgram);
 	r.SetElementType(ctx, VENUS::Render::ELEMENT_TYPE_TRIANGLELIST);
-	r.Draw(ctx, 1);
+	GAIA::MATH::MTX44<GAIA::REAL> mtxRotate, mtxView, mtxProj, mtxCombin;
+	GAIA::REAL rAspect = (GAIA::REAL)r.GetDesc().pCanvas->Size().y / (GAIA::REAL)r.GetDesc().pCanvas->Size().x;
+	static GAIA::REAL s_rRotate = 0.0F;
+	s_rRotate += R(0.001);
+	mtxRotate.rotatex(s_rRotate);
+	mtxView.look(
+		GAIA::MATH::VEC3<GAIA::REAL>(R(0.0), R(0.0), R(-10.0)), 
+		GAIA::MATH::VEC3<GAIA::REAL>(R(0.0), R(0.0), R(1.0)), 
+		GAIA::MATH::VEC3<GAIA::REAL>(R(0.0), R(1.0), R(0.0)));
+	mtxProj.perspective(GAIA::MATH::PI * R(0.5), 1.0F / rAspect, R(0.001), R(1000.0));
+	mtxCombin = mtxRotate * mtxView * mtxProj;
+	r.SetConstant(ctx, "u_mtx", mtxCombin.front_ptr(), 4, 4, 1);
+	r.Draw(ctx, 12);
 
 	// Flush.
 	r.Flush(ctx, GAIA::True);
